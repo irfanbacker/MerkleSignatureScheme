fa5159f5cde60e407ffcc91b5008ef6defeb5597
irfanbacker <irfuvk@gmail.com>
2021-02-22 23:06:21 +0530

    Refactor Lamport OTS module

---
+++ lamport/__init__.py
@@ -0,0 +1,70 @@
+import hashlib
+from binascii import hexlify, unhexlify
+from os import urandom
+
+class LamportSignature:
+    def __init__(self):
+        self.generateKeys()
+        self.used = False
+    
+    def generateKeys(self):
+        self.zeroPrivateKey = [LamportSignature.randomKey() for i in range(256)]
+        self.onePrivateKey = [LamportSignature.randomKey() for i in range(256)]
+        self.zeroPublicKey = [LamportSignature.sha256(b) for b in self.zeroPrivateKey]
+        self.onePublicKey = [LamportSignature.sha256(b) for b in self.onePrivateKey]
+
+    def sign(self, msg):
+        if(self.used): print("Key has already been used")
+        else: self.used = True
+        binaryHashedMsg = LamportSignature.hashMessageToBinary(msg)
+        signature = []
+        print('Signing message for "'+msg+'"')
+        for i in range(len(binaryHashedMsg)):
+            bit = binaryHashedMsg[i]
+            signature.append(self.privateKey[bit][i])
+        return signature
+
+    def verify(self, msg, signature):
+        binaryHashedMsg = LamportSignature.hashMessageToBinary(msg)
+        for i in range(len(binaryHashedMsg)):
+            bit = binaryHashedMsg[i]
+            if(LamportSignature.sha256(signature[i]) != self.publicKey[bit][i]): return False
+        return True
+
+    @property
+    def privateKey(self):
+        return [self.zeroPrivateKey, self.onePrivateKey]
+
+    @property
+    def publicKey(self):
+        return [self.zeroPublicKey, self.onePublicKey]
+
+    @staticmethod
+    def randomKey(n=32):
+        return hexlify(urandom(n)).decode('utf-8')
+
+    @staticmethod
+    def sha256(textValue):
+        return hashlib.sha256(textValue.encode('utf-8')).hexdigest()
+
+    @staticmethod
+    def sha256Bytes(textValue):
+        return hashlib.sha256(textValue.encode('utf-8')).digest()
+
+    @staticmethod
+    def hexToBinary(hashedBytes):
+        binaryMsg= []
+        for byte in hashedBytes:
+                i = 128
+                while(i > 0):
+                    if byte & i != 0:
+                        binaryMsg.append(1)
+                    else:
+                        binaryMsg.append(0)
+                    i = int(i/2)
+        return binaryMsg
+
+    @staticmethod
+    def hashMessageToBinary(msg):
+        hashedBytesMsg = LamportSignature.sha256Bytes(msg)
+        return LamportSignature.hexToBinary(hashedBytesMsg)
--- lamportTest.py
+++ lamportTest.py
@@ -1,7 +1,8 @@
-from lamport import lamport
+import lamport
 
-keys = lamport.generateKeys()
-sig = lamport.signMessage('testing', keys.private)
-print('Signature verification for "testin1": ', lamport.verifyMessage('testin1', keys.public, sig))
-print('Signature verification for "testin1": ', lamport.verifyMessage('testIng', keys.public, sig))
-print('Signature verification for "testing": ', lamport.verifyMessage('testing', keys.public, sig))
+lamportKeys = lamport.LamportSignature()
+sig = lamportKeys.sign('testing123')
+print('Signature verification for "testin1": ', lamportKeys.verify('testin1', sig))
+print('Signature verification for "testIng": ', lamportKeys.verify('testIng', sig))
+print('Signature verification for "testing": ', lamportKeys.verify('testing', sig))
+print('Signature verification for "testing123": ', lamportKeys.verify('testing123', sig))
